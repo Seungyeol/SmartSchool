8058b92e4ea02f6567f7e9876fe4d5a6539fb876
eastflag <eastflag@gmail.com>
2015-08-10 11:23:52 +0900

    gcm noti 추가

--- app/src/main/java/com/aura/smartschool/BR/GcmBroadcastReceiver.java
+++ app/src/main/java/com/aura/smartschool/BR/GcmBroadcastReceiver.java
@@ -1,12 +1,16 @@
 package com.aura.smartschool.BR;
 
-import android.content.BroadcastReceiver;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.text.TextUtils;
-import android.util.Log;
+
+import com.aura.smartschool.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,7 +48,27 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
             //학교 알리미
             if("school".equals(command)) {
+                int category = json.getInt("category");
+                String title = json.getString("title");
+                String content = json.getString("content");
+                String noti_date = json.getString("noti_date");
 
+                int requestID = (int) System.currentTimeMillis();
+                PendingIntent contentIntent = PendingIntent.getActivity(context, requestID, new Intent(),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification noti = new NotificationCompat.Builder(context)
+                        .setContentTitle(title)
+                        .setContentText(content)
+                        .setTicker(title)
+                        .setSmallIcon(R.drawable.school)
+                        .setDefaults(Notification.DEFAULT_SOUND)
+                        .setAutoCancel(true)
+                        .setContentIntent(contentIntent)
+                        .build();
+                nm.notify(8888, noti);
+
+                return;
             }
 
             //학교 상담
