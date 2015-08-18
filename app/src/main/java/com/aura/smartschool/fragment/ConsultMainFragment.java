f58c64d5a2740f7b1686e3685fa39713e6cb778f
seungyeol.bak <syisman84@gmail.com>
2015-08-19 00:21:53 +0900

    상담 전화 연결

--- app/src/main/java/com/aura/smartschool/fragment/ConsultMainFragment.java
+++ app/src/main/java/com/aura/smartschool/fragment/ConsultMainFragment.java
@@ -17,7 +17,7 @@ import com.aura.smartschool.vo.MemberVO;
 /**
  * Created by Administrator on 2015-07-21.
  */
-public class ConsultMainFragment extends Fragment implements View.OnClickListener {
+public class ConsultMainFragment extends Fragment {
     private static String KEY_MEMBER = "member";
     private MemberVO mMember;
 
@@ -30,6 +30,8 @@ public class ConsultMainFragment extends Fragment implements View.OnClickListene
     private View tvBtnPsychology;
     private View tvBtnGrowth;
     private View tvBtnSmoking;
+    private View tvConsultHistory;
+    private View tvConsultCenter;
 
     public static ConsultMainFragment newInstance(MemberVO member) {
         ConsultMainFragment instance = new ConsultMainFragment();
@@ -60,17 +62,21 @@ public class ConsultMainFragment extends Fragment implements View.OnClickListene
         tvBtnPsychology = view.findViewById(R.id.tv_psychology);
         tvBtnGrowth = view.findViewById(R.id.tv_growth);
         tvBtnSmoking = view.findViewById(R.id.tv_smoking);
+        tvConsultHistory = view.findViewById(R.id.tv_consult_history);
+        tvConsultCenter = view.findViewById(R.id.tv_consult_center);
 
-        tvBtnSchoolViolence.setOnClickListener(this);
-        tvBtnFriendRelationship.setOnClickListener(this);
-        tvBtnFamily.setOnClickListener(this);
-        tvBtnSexual.setOnClickListener(this);
-        tvBtnAcademic.setOnClickListener(this);
-        tvBtnCareer.setOnClickListener(this);
-        tvBtnPsychology.setOnClickListener(this);
-        tvBtnGrowth.setOnClickListener(this);
-        tvBtnSmoking.setOnClickListener(this);
-        
+        tvBtnSchoolViolence.setOnClickListener(chatClicked);
+        tvBtnFriendRelationship.setOnClickListener(chatClicked);
+        tvBtnFamily.setOnClickListener(chatClicked);
+        tvBtnSexual.setOnClickListener(chatClicked);
+        tvBtnAcademic.setOnClickListener(chatClicked);
+        tvBtnCareer.setOnClickListener(chatClicked);
+        tvBtnPsychology.setOnClickListener(chatClicked);
+        tvBtnGrowth.setOnClickListener(chatClicked);
+        tvBtnSmoking.setOnClickListener(chatClicked);
+        tvConsultHistory.setOnClickListener(clickListener);
+        tvConsultCenter.setOnClickListener(clickListener);
+
         return view;
     }
 
@@ -80,41 +86,57 @@ public class ConsultMainFragment extends Fragment implements View.OnClickListene
         ((MainActivity)getActivity()).setHeaderView(R.drawable.actionbar_back, mMember.name);
     }
 
-    @Override
-    public void onClick(View v) {
-        ConsultType chatType = null;
-        switch (v.getId()) {
-            case R.id.tv_school_violence:
-                chatType = ConsultType.SCHOOL_VIOLENCE_CONSULT;
-                break;
-            case R.id.tv_friend_relationship:
-                chatType = ConsultType.FRIEND_RELATIONSHIP_CONSULT;
-                break;
-            case R.id.tv_family:
-                chatType = ConsultType.FAMILY_CONSULT;
-                break;
-            case R.id.tv_sexual:
-                chatType = ConsultType.SEXUAL_CONSULT;
-                break;
-            case R.id.tv_academic:
-                chatType = ConsultType.ACADEMIC_CONSULT;
-                break;
-            case R.id.tv_career:
-                chatType = ConsultType.CAREER_CONSULT;
-                break;
-            case R.id.tv_psychology:
-                chatType = ConsultType.PSYCHOLOGY_CONSULT;
-                break;
-            case R.id.tv_growth:
-                chatType = ConsultType.GROWTH_CONSULT;
-                break;
-            case R.id.tv_smoking:
-                chatType = ConsultType.SMOKING_CONSULT;
-                break;
-            default:
-                break;
-        }
+    private View.OnClickListener chatClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ConsultType chatType = null;
+            switch (v.getId()) {
+                case R.id.tv_school_violence:
+                    chatType = ConsultType.SCHOOL_VIOLENCE_CONSULT;
+                    break;
+                case R.id.tv_friend_relationship:
+                    chatType = ConsultType.FRIEND_RELATIONSHIP_CONSULT;
+                    break;
+                case R.id.tv_family:
+                    chatType = ConsultType.FAMILY_CONSULT;
+                    break;
+                case R.id.tv_sexual:
+                    chatType = ConsultType.SEXUAL_CONSULT;
+                    break;
+                case R.id.tv_academic:
+                    chatType = ConsultType.ACADEMIC_CONSULT;
+                    break;
+                case R.id.tv_career:
+                    chatType = ConsultType.CAREER_CONSULT;
+                    break;
+                case R.id.tv_psychology:
+                    chatType = ConsultType.PSYCHOLOGY_CONSULT;
+                    break;
+                case R.id.tv_growth:
+                    chatType = ConsultType.GROWTH_CONSULT;
+                    break;
+                case R.id.tv_smoking:
+                    chatType = ConsultType.SMOKING_CONSULT;
+                    break;
+                default:
+                    break;
+            }
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame, ConsultChattingFragment.newInstance(mMember, chatType)).addToBackStack(null).commit();
-    }
+            getFragmentManager().beginTransaction().replace(R.id.content_frame, ConsultChattingFragment.newInstance(mMember, chatType)).addToBackStack(null).commit();
+        }
+    };
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.tv_consult_history:
+                    break;
+                case R.id.tv_consult_center:
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "15441284"));
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 }
