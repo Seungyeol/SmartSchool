0b173fa31162780aa4a3a5a5ba25c80734063ab8
seungyeol.bak <syisman84@gmail.com>
2015-09-08 22:01:50 +0900

    티쳐티쳐 => 상담원

--- app/src/main/java/com/aura/smartschool/adapter/ConsultChattingAdapter.java
+++ app/src/main/java/com/aura/smartschool/adapter/ConsultChattingAdapter.java
@@ -143,7 +143,7 @@ public class ConsultChattingAdapter extends RecyclerView.Adapter<ConsultChatting
         @Override
         public void onBindViewHolder(ConsultVO msg, boolean isFirstMsgOfDay, boolean isFailList) {
             super.onBindViewHolder(msg, isFirstMsgOfDay, isFailList);
-            tvName.setText("티쳐티쳐");
+            tvName.setText("상담원");
         }
     }
 
