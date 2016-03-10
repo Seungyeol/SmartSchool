479e2ec8a56c2e9a42e3097b1aaf2825876b9fa0
eastflag <eastflag@gmail.com>
2016-03-10 14:01:45 +0900

    위치 서비스 부팅서비스에 등록

--- app/src/main/java/com/aura/smartschool/receiver/BootCompleteReceiver.java
+++ app/src/main/java/com/aura/smartschool/receiver/BootCompleteReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.aura.smartschool.service.MyLocationService;
 import com.aura.smartschool.service.SOSIconService;
 import com.aura.smartschool.service.StepCounterService;
 import com.aura.smartschool.utils.PreferenceUtil;
@@ -17,17 +18,23 @@ public class BootCompleteReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        //SOS 서비스 시작
-        if (PreferenceUtil.getInstance(context).isSOSEnabled()) {
-            Intent sosIntent = new Intent(context, SOSIconService.class);
-            context.startService(sosIntent);
-        }
+        //부모의 경우는 위치서비스와 활동량 서비스를 하지 않는다.
+        if (!PreferenceUtil.getInstance(context).isParent()) {
+            //SOS 서비스 시작
+            if (PreferenceUtil.getInstance(context).isSOSEnabled()) {
+                Intent sosIntent = new Intent(context, SOSIconService.class);
+                context.startService(sosIntent);
+            }
 
-        //Step Counter 서비스 시작
-        if (Util.isKitkatWithStepSensor(context) && !PreferenceUtil.getInstance(context).isParent()) {
-            StepSharePrefrenceUtil.saveMergeStepCount(context, StepSharePrefrenceUtil.getCurrentStepCount(context));
-            StepSharePrefrenceUtil.saveDiffStepCount(context, 0);
-            context.startService(new Intent(context, StepCounterService.class));
+            //Step Counter 서비스 시작
+            if (Util.isKitkatWithStepSensor(context)) {
+                StepSharePrefrenceUtil.saveMergeStepCount(context, StepSharePrefrenceUtil.getCurrentStepCount(context));
+                StepSharePrefrenceUtil.saveDiffStepCount(context, 0);
+                context.startService(new Intent(context, StepCounterService.class));
+            }
+
+            //위치 서비스 시작
+            context.startService(new Intent(context, MyLocationService.class));
         }
     }
 }
